a7ddfc8ea7b0c682aaca7dde333585d4a7c5109e
Catalin Waack <cata.waack@gmail.com>
2023-06-13 19:38:40 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Image from "next/image";
-import styles from '../styles/Home.module.css'
-import { useState, useMemo } from 'react';
-import MicRecorder from 'mic-recorder-to-mp3';
+import styles from "../styles/Home.module.css";
+import { useState, useMemo } from "react";
+import MicRecorder from "mic-recorder-to-mp3";
 
 const Home = () => {
-
   const [audio, setAudio] = useState();
   const [transcript, setTranscript] = useState();
   const [loading, setLoading] = useState(false);
@@ -18,7 +17,7 @@ const Home = () => {
 
   const startRecording = () => {
     if (isBlocked) {
-      console.log('Permission Denied');
+      console.log("Permission Denied");
       setIsBlocked(true);
     } else {
       recorder
@@ -26,10 +25,9 @@ const Home = () => {
         .then(() => {
           setIsRecording(true);
         })
-        .catch(e => console.error(e));
+        .catch((e) => console.error(e));
     }
-  }
-
+  };
 
   const stopRecording = () => {
     setIsRecording(false);
@@ -37,9 +35,9 @@ const Home = () => {
       .stop()
       .getMp3()
       .then(([buffer, blob]) => {
-        const file = new File(buffer, 'test.mp3', {
+        const file = new File(buffer, "test.mp3", {
           type: blob.type,
-          lastModified: Date.now()
+          lastModified: Date.now(),
         });
         setBlobURL(URL.createObjectURL(file));
         // Convert to base64
@@ -48,71 +46,106 @@ const Home = () => {
         reader.onloadend = function () {
           const base64data = reader.result;
           // Only send the base64 string
-          const base64String = base64data.split(',')[1];
+          const base64String = base64data.split(",")[1];
           setAudio(base64String);
-        }
-      }
-      )
-      }
-      
-      const handleSubmit = async(e) => {
-        e.preventDefault();
-        setLoading(true);
-        setIsRecording(false);
-        
-        const response = await fetch("/api/whisper", {
-          method: "POST",
-          headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ audio: audio }),
+        };
       });
-      
-      const data = await response.json();
-      setLoading(false);
-      setTranscript(data.modelOutputs[0].text);
-
   };
-  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setIsRecording(false);
+
+    const response = await fetch("/api/whisper", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ audio: audio }),
+    });
+
+    const data = await response.json();
+    setLoading(false);
+    setTranscript(data.modelOutputs[0].text);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>OpenAI Whisper Demo</title>
+        <title>OpenAI Whisper Demo from Cata</title>
         <meta name="description" content="OpenAI Whisper Next.js Template" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className = {styles.banner}>
-        <p>Next.js template available on <a href="https://github.com/zahidkhawaja/whisper-nextjs" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+      <div className={styles.banner}>
+        <p>
+          Next.js template available on{" "}
+          <a
+            href="https://github.com/zahidkhawaja/whisper-nextjs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
       </div>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Whisper 🤫
-        </h1>
+        <h1 className={styles.title}>Whisper 🤫</h1>
 
-        <p className={styles.description}> Record audio to generate a transcript. </p>
-        {isRecording ? <p className={styles.warning}> Recording in progress... </p> : <p className={styles.warning}> Requires browser microphone permission. </p>}
-        {isBlocked ? <p className={styles.blocked}> Microphone access is blocked. </p> : null}
+        <p className={styles.description}>
+          {" "}
+          Record audio to generate a transcript.{" "}
+        </p>
+        {isRecording ? (
+          <p className={styles.warning}> Recording in progress... </p>
+        ) : (
+          <p className={styles.warning}>
+            {" "}
+            Requires browser microphone permission.{" "}
+          </p>
+        )}
+        {isBlocked ? (
+          <p className={styles.blocked}> Microphone access is blocked. </p>
+        ) : null}
 
         <div className={styles.whispercontainer}>
-          
-          <div className = {styles.allbuttons}>
-       <button onClick = {startRecording} disabled = {isRecording} className = {styles.recordbutton}>Record</button>
-       <button onClick = {stopRecording} disabled = {!isRecording} className = {styles.stopbutton}>Stop</button>
-       </div>
+          <div className={styles.allbuttons}>
+            <button
+              onClick={startRecording}
+              disabled={isRecording}
+              className={styles.recordbutton}
+            >
+              Record
+            </button>
+            <button
+              onClick={stopRecording}
+              disabled={!isRecording}
+              className={styles.stopbutton}
+            >
+              Stop
+            </button>
+          </div>
 
-      <div className = {styles.audiopreview}>
-       <audio src={blobURL} controls="controls" />
-       </div>
-       <div className = {styles.loading}>
-       {loading ? <p>Loading... please wait.</p> :  <p>{transcript}</p>}
-       </div>
-          <div className = {styles.generatebuttonroot}>
-            <button type = "submit" className = {styles.generatebutton} onClick = {handleSubmit} disabled = {!audio}>Generate</button>
-            </div>
-            </div>
-            </main>
+          <div className={styles.audiopreview}>
+            <audio src={blobURL} controls="controls" />
+          </div>
+          <div className={styles.loading}>
+            {loading ? <p>Loading... please wait.</p> : <p>{transcript}</p>}
+          </div>
+          <div className={styles.generatebuttonroot}>
+            <button
+              type="submit"
+              className={styles.generatebutton}
+              onClick={handleSubmit}
+              disabled={!audio}
+            >
+              Generate
+            </button>
+          </div>
+        </div>
+      </main>
 
       <footer className={styles.footer}>
         <a
@@ -120,14 +153,14 @@ const Home = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/banana.svg" alt="Banana Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
